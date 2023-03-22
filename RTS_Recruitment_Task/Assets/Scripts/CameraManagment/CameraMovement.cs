ad4b489c439748c7a4d2a4876a43a96f3e5071a1
Krystian Szlagor <krystian.szlagor@4experience.co>
2023-03-22 17:27:30 +0100

    Created CameraMovement script

---
+++ RTS_Recruitment_Task/Assets/Scripts/CameraManagment/CameraMovement.cs
@@ -0,0 +1,62 @@
+using Recruitment.InputMangment;
+using UnityEngine;
+
+namespace Recruitment.CamerManagment
+{
+    public class CameraMovement : MonoBehaviour
+    {
+        [field: SerializeField]
+        private PlayerInput CurrentInput { get; set; }
+
+        [field: Header("Movement settings")]
+        [field: SerializeField]
+        private float MoveSpeed { get; set; }
+        [field: SerializeField]
+        private float RotateSpeed { get; set; }
+        [field: SerializeField]
+        private float MinX { get; set; }
+        [field: SerializeField]
+        private float MaxX { get; set; }
+        [field: SerializeField]
+        private float MinZ { get; set; }
+        [field: SerializeField]
+        private float MaxZ { get; set; }
+
+        private float RotationY { get; set; }
+
+
+        protected virtual void Update ()
+        {
+            ChangeCameraPosition();
+            ChangeCameraRotation();
+        }
+
+        private void ChangeCameraPosition ()
+        {
+            Vector2 moveAxis = CurrentInput.GetMoveVector();
+
+            if (moveAxis != Vector2.zero)
+            {
+                Vector3 originPosition = transform.position;
+
+                originPosition += moveAxis.x * MoveSpeed * Time.deltaTime * transform.right;
+                originPosition += moveAxis.y * MoveSpeed * Time.deltaTime * transform.forward;
+                originPosition.x = Mathf.Clamp(originPosition.x, MinX, MaxX);
+                originPosition.z = Mathf.Clamp(originPosition.z, MinZ, MaxZ);
+
+                transform.position = originPosition;
+            }
+        }
+
+        private void ChangeCameraRotation ()
+        {
+            float rotateAxis = CurrentInput.GetRotateVector();
+
+            if (rotateAxis != 0.0f)
+            {
+                RotationY += rotateAxis * Time.deltaTime * RotateSpeed;
+                transform.rotation = Quaternion.Euler(0.0f, RotationY, 0.0f);
+            }
+        }
+    }
+}
