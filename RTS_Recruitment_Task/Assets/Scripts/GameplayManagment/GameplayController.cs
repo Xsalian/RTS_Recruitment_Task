55dd0afbe5b4cc3f7a8ac91d48d6523f0b187b39
Krystian Szlagor <krystian.szlagor@4experience.co>
2023-03-23 15:57:00 +0100

    Created GameplayController script

---
+++ RTS_Recruitment_Task/Assets/Scripts/GameplayManagment/GameplayController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Recruitment.GameplayManagment
+{
+    public class GameplayController : MonoBehaviour
+    {
+        public bool IsKeyCollected { get; set; }
+    }
+}
