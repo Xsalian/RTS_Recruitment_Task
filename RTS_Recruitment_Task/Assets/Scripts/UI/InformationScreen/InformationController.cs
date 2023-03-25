52f825beb585d08b06fecba25479352333936ad5
Krystian Szlagor <krystian.szlagor@4experience.co>
2023-03-25 01:05:30 +0100

    Added logic to disable player mechanics during visible panel in InformationController script

--- RTS_Recruitment_Task/Assets/Scripts/UI/InformationScreen/InformationController.cs
+++ RTS_Recruitment_Task/Assets/Scripts/UI/InformationScreen/InformationController.cs
@@ -1,18 +1,25 @@
+using Recruitment.GameplayManagment;
 using Recruitment.InteractionManagment;
 using Recruitment.MVC;
+using UnityEngine;
 
 namespace Recruitment.UI
 {
     public class InformationController : Controller<InformationView, InformationModel>
     {
+        [field: SerializeField]
+        private GameplayController CurrentGameplayController { get; set; }
+
         public void DisplayPanel (IInteractable interactable)
         {
+            CurrentGameplayController.SetPlayerMechanicsEnableState(false);
             gameObject.SetActive(true);
             CurrentView.SetLabelText(interactable.InformationText);
         }
 
         public void ClosePanel ()
         {
+            CurrentGameplayController.SetPlayerMechanicsEnableState(true);
             gameObject.SetActive(false);
         }
     }
