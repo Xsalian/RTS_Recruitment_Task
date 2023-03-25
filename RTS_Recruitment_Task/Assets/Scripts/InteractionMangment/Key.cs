7381cbe4de78b9e55385966164919565a1ffc81c
Krystian Szlagor <krystian.szlagor@4experience.co>
2023-03-25 16:34:45 +0100

    Implemented interface, add list of renderer in Key script

--- RTS_Recruitment_Task/Assets/Scripts/InteractionMangment/Key.cs
+++ RTS_Recruitment_Task/Assets/Scripts/InteractionMangment/Key.cs
@@ -1,4 +1,7 @@
 using Recruitment.GameplayManagment;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
 namespace Recruitment.InteractionManagment
@@ -9,9 +12,12 @@ namespace Recruitment.InteractionManagment
         public string ConfirmationText { get; set; }
         [field: SerializeField]
         public string InformationText { get; set; }
-
         [field: SerializeField]
         private GameplayController CurrentGameplayController { get; set; }
+        [field: SerializeField]
+        private List<Renderer> RendererCollection { get; set; } = new();
+        [field: SerializeField]
+        private Highlighter CurrentHighlighter { get; set; }
 
         public void Interaction ()
         {
@@ -22,6 +28,16 @@ namespace Recruitment.InteractionManagment
         public bool CanInteract ()
         {
             return true;
+        }
+
+        public void Highlight()
+        {
+            CurrentHighlighter.ChangeHighlightColor(RendererCollection, true);
+        }
+
+        public void StopHighlight()
+        {
+            CurrentHighlighter.ChangeHighlightColor(RendererCollection, false);
         }
     }
 }
