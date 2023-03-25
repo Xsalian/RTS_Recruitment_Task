7e4841784d9a3fc2dfb77d2251ef94bb53446423
Krystian Szlagor <krystian.szlagor@4experience.co>
2023-03-25 16:37:11 +0100

    Added logic to call highlight on interactable objects in MouseRayController script

--- RTS_Recruitment_Task/Assets/Scripts/InteractionMangment/MouseRayController.cs
+++ RTS_Recruitment_Task/Assets/Scripts/InteractionMangment/MouseRayController.cs
@@ -12,32 +12,61 @@ namespace Recruitment.InteractionManagment
         private InformationController InformationPanel { get; set; }
         [field: SerializeField]
         private Camera MainCamera { get; set; }
+        [field: SerializeField]
+        private float MaxRayDistance { get; set; }
+        [field: SerializeField]
+        private LayerMask InteractableLayer { get; set; }
+
+        private IInteractable CurrentInteractable { get; set; }
 
         protected virtual void Update ()
         {
             CastRay();
+            SelectInteractable();
         }
 
         private void CastRay ()
         {
             Ray ray = MainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
 
-            if (Physics.Raycast(ray, out RaycastHit hit) == true)
+            if (Physics.Raycast(ray, out RaycastHit hit, MaxRayDistance, InteractableLayer) == true)
             {
-                if (Mouse.current.leftButton.wasPressedThisFrame == true)
+                IInteractable cachedInteractable = hit.collider.GetComponent<IInteractable>();
+
+                if (cachedInteractable != null && cachedInteractable != CurrentInteractable)
                 {
-                    IInteractable cachedInteractable = hit.collider.GetComponent<IInteractable>();
-                    
-                    if (cachedInteractable != null)
+                    if (CurrentInteractable != null)
                     {
-                        if (cachedInteractable.CanInteract() == true)
-                        {
-                            ConfirmationPanel.DisplayConfirmationPanel(cachedInteractable);
-                        }
-                        else
-                        {
-                            InformationPanel.DisplayPanel(cachedInteractable);
-                        }
+                        CurrentInteractable.StopHighlight();
+                    }
+
+                    CurrentInteractable = cachedInteractable;
+                    CurrentInteractable.Highlight();
+                }
+            }
+            else
+            {
+                if (CurrentInteractable != null)
+                {
+                    CurrentInteractable.StopHighlight();
+                    CurrentInteractable = null;
+                }
+            }
+        }
+
+        private void SelectInteractable ()
+        {
+            if (Mouse.current.leftButton.wasPressedThisFrame == true)
+            {
+                if (CurrentInteractable != null)
+                {
+                    if (CurrentInteractable.CanInteract() == true)
+                    {
+                        ConfirmationPanel.DisplayConfirmationPanel(CurrentInteractable);
+                    }
+                    else
+                    {
+                        InformationPanel.DisplayPanel(CurrentInteractable);
                     }
                 }
             }
