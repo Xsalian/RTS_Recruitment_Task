5eb38a63aa2019132e7700726d0424b44e9c8623
Krystian Szlagor <krystian.szlagor@4experience.co>
2023-03-25 01:05:45 +0100

    Added logic to disable player mechanics during visible panel in ConfirmationController script

--- RTS_Recruitment_Task/Assets/Scripts/UI/ConfirmationScreen/ConfirmationController.cs
+++ RTS_Recruitment_Task/Assets/Scripts/UI/ConfirmationScreen/ConfirmationController.cs
@@ -1,12 +1,18 @@
+using Recruitment.GameplayManagment;
 using Recruitment.InteractionManagment;
 using Recruitment.MVC;
+using UnityEngine;
 
 namespace Recruitment.UI
 {
     public class ConfirmationController : Controller<ConfirmationView, ConfirmationModel>
     {
+        [field: SerializeField]
+        private GameplayController CurrentGameplayController { get; set; }
+
         public void DisplayConfirmationPanel (IInteractable interactable)
         {
+            CurrentGameplayController.SetPlayerMechanicsEnableState(false);
             gameObject.SetActive(true);
             CurrentModel.SetCurrentInteractable(interactable);
             CurrentView.SetLabelText(interactable.ConfirmationText);
@@ -14,12 +20,14 @@ namespace Recruitment.UI
 
         public void ConfirmAction ()
         {
+            CurrentGameplayController.SetPlayerMechanicsEnableState(true);
             CurrentModel.ConfirmInteraction();
             gameObject.SetActive(false);
         }
 
         public void CloseConfirmationPanel ()
         {
+            CurrentGameplayController.SetPlayerMechanicsEnableState(true);
             gameObject.SetActive(false);
         }
     }
