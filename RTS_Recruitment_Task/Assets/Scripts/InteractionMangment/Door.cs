f543dfb5f4febc95dbd657ae36b04b13a474a221
Krystian Szlagor <krystian.szlagor@4experience.co>
2023-03-25 01:10:34 +0100

    Added logic reponsible for play idle animation on game end in Door script

--- RTS_Recruitment_Task/Assets/Scripts/InteractionMangment/Door.cs
+++ RTS_Recruitment_Task/Assets/Scripts/InteractionMangment/Door.cs
@@ -9,22 +9,48 @@ namespace Recruitment.InteractionManagment
         public string ConfirmationText { get; set; }
         [field: SerializeField]
         public string InformationText { get; set; }
-
         [field: SerializeField]
         private GameplayController CurrentGameplayController { get; set; }
         [field: SerializeField]
         private Animator CurrentAnimator { get; set; }
 
-        private const string OPEN_DOOR_TRIGGER = "CanOpenDoor";
+        private const string OPEN_DOOR_TRIGGER = "OpenDoor";
+        private const string IDLE_DOOR_TRIGGER = "ResetDoor";
 
         public void Interaction ()
         {
             CurrentAnimator.SetTrigger(OPEN_DOOR_TRIGGER);
+            CurrentGameplayController.EndGame();
         }
 
         public bool CanInteract ()
         {
             return CurrentGameplayController.IsKeyCollected == true;
+        }
+
+        protected virtual void Start()
+        {
+            AttachToEvent();
+        }
+
+        protected virtual void OnDestroy()
+        {
+            DetachFromEvent();
+        }
+
+        private void AttachToEvent()
+        {
+            CurrentGameplayController.GameEnded += HandleOnGameEnded;
+        }
+
+        private void DetachFromEvent()
+        {
+            CurrentGameplayController.GameEnded -= HandleOnGameEnded;
+        }
+
+        private void HandleOnGameEnded()
+        {
+            CurrentAnimator.SetTrigger(IDLE_DOOR_TRIGGER);
         }
     }
 }
