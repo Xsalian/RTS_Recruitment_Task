c4e56db4a900aeeda4137dbaaa394d8f07a82b7b
Krystian Szlagor <krystian.szlagor@4experience.co>
2023-03-27 07:57:11 +0200

    Set property access modifiers to private in ConfirmationView

--- RTS_Recruitment_Task/Assets/Scripts/UI/ConfirmationScreen/ConfirmationView.cs
+++ RTS_Recruitment_Task/Assets/Scripts/UI/ConfirmationScreen/ConfirmationView.cs
@@ -7,7 +7,7 @@ namespace Recruitment.UI
     public class ConfirmationView : View
     {
         [field: SerializeField]
-        public TextMeshProUGUI Label { get; set; }
+        private TextMeshProUGUI Label { get; set; }
 
         public void SetLabelText (string targetText)
         {
